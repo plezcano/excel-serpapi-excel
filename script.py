0b15670abd6bd2d00c589f7e54d1377fed641ba5
plezcano <plezcanovilchez@hotmail.com>
2026-04-30 21:54:52 -0700

    Update script.py

--- script.py
+++ script.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 print("inicio")
 
-df = pd.read_excel("3. SEO_Website_DLS.xlsx")
+# Leer Excel
+df = pd.read_excel("3. SEO_Website_DLS.xlsx", header=0)
 
-print("filas:", df.shape)
-
+# Eliminar filas sin keyword
 df = df.dropna(subset=["keyword"])
 
-print("filas limpias:", df.shape)
+# Resetear índice
+df = df.reset_index(drop=True)
 
+# Mostrar info
+print("filas limpias:", df.shape)
 print(df.head())
 
 print("fin")
