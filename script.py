3976651b5a7fe4e6b353aad7a4bba646effade76
plezcano <plezcanovilchez@hotmail.com>
2026-04-30 21:27:32 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,25 +1,7 @@
-import pandas as pd
-import requests
+import time
 
 print("inicio script")
 
-# ejemplo base
-data = [
-    {"keyword": "pool service indio"}
-]
-
-for row in data:
-    keyword = row["keyword"]
-
-    url = "https://serpapi.com/search.json"
-    params = {
-        "q": keyword,
-        "api_key": "b42c2dc107e6b2487a8ec59fb482c65392ec8dd841829e0e3588b08e93d7bcfd"
-    }
-
-    response = requests.get(url, params=params)
-    result = response.json()
-
-    print(result.get("organic_results", [])[:1])
+time.sleep(5)
 
 print("fin script")
